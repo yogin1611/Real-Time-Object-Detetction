eba4b426a12d23d998385b23049dbe235a93830e
Heerath Bhat <heerath.bhat@gmail.com>
2024-12-29 20:43:26 +0530

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <main className="font-mono flex flex-col justify-center items-center  w-screen">
-        <h1 className="m-5 text-xl font-bold">Real-Time Object Detection</h1>
+        <h1 className="m-5 text-xl font-bold">Real-Time Object Detection-Mini Project</h1>
         <Yolo />
         
       </main>
